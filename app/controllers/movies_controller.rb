637b9053491efc37be69a8e4f4b28f8e4e696b3a
jcarpe60 <jcarpe60@uncc.edu>
2015-10-29 14:29:48 +0000

    Incomplete HW 2 Part 2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,15 +11,73 @@ class MoviesController < ApplicationController
   end
   
   def index
-    #@movies = Movie.all
-    @movies, @class1, @class2 = case params[:sort_by]
-    when "title"
-      [Movie.all.order(params[:sort_by]), "hilite", "not"]
-    when "release_date"
-      [Movie.all.order(params[:sort_by]), "not", "hilite"]
+    @all_ratings = Movie.all_ratings
+
+    #debugger
+    session[:ratings] = params[:ratings] unless params[:ratings].nil?
+    session[:sort_by] = params[:sort_by] unless params[:sort_by].nil?
+    
+    @selected = session[:ratings]
+    @sorting = session[:sort_by]
+     
+    if @selected.present? && @sorting.present?
+      @movies, @class1, @class2 = case params[:sort_by]
+      when "title"
+        [Movie.where(rating: @selected.keys).order(@sorting), "hilite", "not"]
+      when "release_date"
+        [Movie.where(rating: @selected.keys).order(@sorting), "not", "hilite"]
+      else
+        [Movie.all, "not", "not"]
+      end 
+    elsif @selected.present? && @sorting.nil?
+      @movies = Movie.where(rating: @selected.keys)
+    elsif @selected.nil? && @sorting.present?
+      @movies, @class1, @class2 = case @sorting
+      when "title"
+        [Movie.all.order(@sorting), "hilite", "not"]
+      when "release_date"
+        [Movie.all.order(@sorting), "not", "hilite"]
+      else
+        [Movie.all, "not", "not"]
+      end 
     else
-      [Movie.all, "not", "not"]
+      @movies = Movie.all
     end 
+     
+    
+=begin
+    if params[:ratings].present?
+      @movies = Movie.where(rating: params[:ratings].keys)
+    else
+      @movies = Movie.all
+    end
+=end
+=begin
+    if params[:ratings].present? && params[:sort_by].present?
+      @movies, @class1, @class2 = case params[:sort_by]
+      when "title"
+        [Movie.where(rating: params[ratings].keys).order(params[:sort_by]), "hilite", "not"]
+      when "release_date"
+        [Movie.where(rating: params[ratings].keys).order(params[:sort_by]), "not", "hilite"]
+      else
+        [Movie.all, "not", "not"]
+      end 
+    elsif params[:ratings].present? && params[:sort_by].nil?
+      @movies = Movie.where(rating: params[:ratings].keys)
+    elsif params[:ratings].nil? && params[:sort_by].present?
+      @movies, @class1, @class2 = case params[:sort_by]
+      when "title"
+        [Movie.all.order(params[:sort_by]), "hilite", "not"]
+      when "release_date"
+        [Movie.all.order(params[:sort_by]), "not", "hilite"]
+      else
+        [Movie.all, "not", "not"]
+      end 
+    else
+      @movies = Movie.all
+    end
+=end
+
   end
 
   def new
